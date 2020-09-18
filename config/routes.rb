35b7f785380d74a14d20342f01660daded356f01
Abeilles <afrojack.dj0628@gmail.com>
2020-09-18 18:18:50 +0900

    [Add]form_for & login_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root :to => 'books#index'
+
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+
+  get "books/:id" => 'books#show', as: 'book'
+
+
 end
